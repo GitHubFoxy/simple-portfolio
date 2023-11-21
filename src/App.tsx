2bb3a65a5422e33abe3ebeaee3bc5f39cad5a39a
ambrosayos <ambrosayos@mail.ru>
2023-11-21 12:56:48 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+      <>
+          <h1 className="text-center">Третьяков Михаил</h1>
+          <h2 className="text-center">Я web-разработчик, 20 лет</h2>
+          <h2 className="text-center">Вот мой последний проект</h2>
+          <div className="flex justify-center"><a className="text-center text-purple-700" href="https://www.youtube.com" target="_blank">Редизайн веб-сайта ресторана "Иероглиф"</a></div>
+      </>
+  )
+}
+
+export default App
